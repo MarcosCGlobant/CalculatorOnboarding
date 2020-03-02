4e6a27254c7c3feef589b6b79d1f2d728af3f0af
Marcos Cifuentes <marcos.cifuentes@globant.com>
2020-03-02 13:47:08 -0300

    added the basic funcionality to show numbers and operations on the emulator

--- app/app/src/main/java/com/globant/calculator/android/mvp/model/CalculatorModel.java
+++ app/app/src/main/java/com/globant/calculator/android/mvp/model/CalculatorModel.java
@@ -1,6 +1,41 @@
 package com.globant.calculator.android.mvp.model;
 
+import static com.globant.calculator.android.utils.Constants.EMPTY_STRING;
+
 public class CalculatorModel {
 
+    private String firstNumber = EMPTY_STRING;
+    private String secondNumber = EMPTY_STRING;
+    private String operator = EMPTY_STRING;
+
+    public String getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setFirstNumber(String firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public String getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(String secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public void clear(){
+        firstNumber = EMPTY_STRING;
+        secondNumber = EMPTY_STRING;
+        operator = EMPTY_STRING;
+    }
 
 }
--- app/app/src/main/java/com/globant/calculator/android/mvp/presenter/CalculatorPresenter.java
+++ app/app/src/main/java/com/globant/calculator/android/mvp/presenter/CalculatorPresenter.java
@@ -3,6 +3,14 @@ package com.globant.calculator.android.mvp.presenter;
 import com.globant.calculator.android.mvp.model.CalculatorModel;
 import com.globant.calculator.android.mvp.view.CalculatorView;
 
+import static com.globant.calculator.android.utils.Constants.DIVIDE;
+import static com.globant.calculator.android.utils.Constants.EMPTY_STRING;
+import static com.globant.calculator.android.utils.Constants.MINUS;
+import static com.globant.calculator.android.utils.Constants.MULTIPLY;
+import static com.globant.calculator.android.utils.Constants.NUMBER_ZERO;
+import static com.globant.calculator.android.utils.Constants.PLUS;
+import static com.globant.calculator.android.utils.Constants.ZERO_INT;
+
 public class CalculatorPresenter {
 
     private CalculatorModel model;
@@ -11,6 +19,55 @@ public class CalculatorPresenter {
     public CalculatorPresenter(CalculatorModel model, CalculatorView view) {
         this.model = model;
         this.view = view;
+    }
+
+    public void onClearButtonPress() {
+        model.clear();
+        view.showOperationPressed(NUMBER_ZERO);
+        view.showResult(ZERO_INT);
+    }
+
+    public void onAddButtonPressed(String symbol) {
+        model.setOperator(symbol);
+        view.showOperationPressed(PLUS);
+    }
+
+    public void onMultiplyButtonPressed(String symbol) {
+        model.setOperator(symbol);
+        view.showOperationPressed(MULTIPLY);
+    }
+
+    public void onSubtractionButtonPressed(String symbol) {
+        model.setOperator(symbol);
+        view.showOperationPressed(MINUS);
+    }
+
+    public void onDivideButtonPressed(String symbol) {
+        model.setOperator(symbol);
+        view.showOperationPressed(DIVIDE);
+    }
+
+    public void onEqualsButtonPressed(String symbol) {
+
+    }
+
+    public void onNumberButtonPress(String number){
+        if ((model.getOperator().equals(EMPTY_STRING) ) && (!model.getFirstNumber().equals(EMPTY_STRING) )){
+            model.setFirstNumber(model.getFirstNumber() + number);
+            view.showNumberPressed(model.getFirstNumber());
+        }
+        else if ((!model.getOperator().equals(EMPTY_STRING)) && (!model.getSecondNumber().equals(EMPTY_STRING))){
+            model.setSecondNumber(model.getSecondNumber() + number);
+            view.showNumberPressed(model.getSecondNumber());
+        }
+        else if ((!model.getOperator().equals(EMPTY_STRING)) && (model.getSecondNumber().equals(EMPTY_STRING))){
+            model.setSecondNumber(number);
+            view.showNumberPressed(model.getSecondNumber());
+        }
+        else {
+            model.setFirstNumber(number);
+            view.showNumberPressed(model.getFirstNumber());
+        }
     }
 
 }
